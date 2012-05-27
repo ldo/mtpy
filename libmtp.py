eab36ba6fa3e610760808bd5414a603484379747
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2012-05-27 10:17:27 +0000

    rework to use wrapper class

--- libmtp.py
+++ libmtp.py
@@ -1,6 +1,15 @@
 import ctypes as ct
 
+import sys # debug
+
 mtp = ct.cdll.LoadLibrary("libmtp.so")
+mtp.LIBMTP_Get_Manufacturername.restype = ct.c_char_p
+mtp.LIBMTP_Get_Modelname.restype = ct.c_char_p
+mtp.LIBMTP_Get_Serialnumber.restype = ct.c_char_p
+mtp.LIBMTP_Get_Deviceversion.restype = ct.c_char_p
+mtp.LIBMTP_Get_Friendlyname.restype = ct.c_char_p
+mtp.LIBMTP_Get_Syncpartner.restype = ct.c_char_p
+mtp.LIBMTP_Get_Filetype_Description.restype = ct.c_char_p
 
 ERROR_NONE = 0
 ERROR_GENERAL = 1
@@ -32,6 +41,12 @@ class Error(RuntimeError) :
     #end __init__
 
 #end Error
+
+def check_status(status) :
+    if status != ERROR_NONE :
+        raise Error(status)
+    #end if
+#end check_status
 
 class error_t(ct.Structure) :
     pass
@@ -76,6 +91,8 @@ device_extension_t._fields_ = \
         ("next", ct.POINTER(device_extension_t)), # Pointer to the next extension or NULL if this is the last extension.
     ]
 
+filetype_t = ct.c_uint
+
 class mtpdevice_t(ct.Structure) :
     pass
 #end mtpdevice_t
@@ -101,36 +118,30 @@ mtpdevice_t._fields_ = \
         ("next", ct.POINTER(mtpdevice_t)), # Pointer to next device in linked list; NULL if this is the last device
     ]
 
-def Get_Connected_Devices() :
-    devices = ct.POINTER(mtpdevice_t)()
-    status = mtp.LIBMTP_Get_Connected_Devices(ct.byref(devices))
-    if status != ERROR_NONE :
-        raise Error(status)
-    #end if
-    dev = devices
-    result = []
-    while bool(dev) :
-        status = mtp.LIBMTP_Get_Storage(dev, LIBMTP_STORAGE_SORTBY_NOTSORTED)
-        if status != ERROR_NONE :
-            raise Error(status)
-        #end if
-        dev = dev.contents
-        item = dict \
-          (
-            (k, getattr(dev, k))
-            for k in
-                (
-                    "object_bitsize", "maximum_battery_level", "default_music_folder",
-                    "default_playlist_folder", "default_picture_folder",
-                    "default_video_folder", "default_organizer_folder",
-                    "default_zencast_folder", "default_album_folder", "default_text_folder",
-                )
-          )
-        storage = []
-        sto = dev.storage
+connected_devices = ct.POINTER(mtpdevice_t)()
+
+class Device() :
+
+    def __init__(self, device) :
+        self.device = device
+        check_status(mtp.LIBMTP_Get_Storage(device, LIBMTP_STORAGE_SORTBY_NOTSORTED))
+        for \
+            k \
+        in \
+            (
+                "object_bitsize", "maximum_battery_level", "default_music_folder",
+                "default_playlist_folder", "default_picture_folder",
+                "default_video_folder", "default_organizer_folder",
+                "default_zencast_folder", "default_album_folder", "default_text_folder",
+            ) \
+        :
+            setattr(self, k, getattr(device.contents, k))
+        #end for
+        self.storage = []
+        sto = device.contents.storage
         while bool(sto) :
             sto = sto.contents
-            storage.append \
+            self.storage.append \
               (
                 dict
                   (
@@ -145,12 +156,11 @@ def Get_Connected_Devices() :
               )
             sto = sto.next
         #end while
-        item["storage"] = storage
-        extensions = []
-        ext = dev.extensions
+        self.extensions = []
+        ext = device.contents.extensions
         while bool(ext) :
             ext = ext.contents
-            extensions.append \
+            self.extensions.append \
               (
                 dict
                   (
@@ -160,10 +170,92 @@ def Get_Connected_Devices() :
               )
             ext = ext.next
         #end while
-        item["extensions"] = extensions
-        result.append(item)
-        dev = dev.next
+    #end __init__
+
+    def get_manufacturer_name(self) :
+        return bytes(mtp.LIBMTP_Get_Manufacturername(self.device)).decode("utf-8")
+    #end get_manufacturer_name
+
+    def get_model_name(self) :
+        return bytes(mtp.LIBMTP_Get_Modelname(self.device)).decode("utf-8")
+    #end get_model_name
+
+    def get_serial_number(self) :
+        return bytes(mtp.LIBMTP_Get_Serialnumber(self.device)).decode("utf-8")
+    #end get_serial_number
+
+    def get_device_version(self) :
+        return bytes(mtp.LIBMTP_Get_Deviceversion(self.device)).decode("utf-8")
+    #end get_device_version
+
+    def get_friendly_name(self) :
+        return bytes(mtp.LIBMTP_Get_Friendlyname(self.device)).decode("utf-8")
+    #end get_friendly_name
+
+    def set_friendly_name(self, new_name) :
+        check_status(mtp.LIBMTP_Set_Friendlyname(self.device, new_name.encode("utf-8")))
+    #end set_friendly_name
+
+    def get_sync_parner(self) :
+        return bytes(mtp.LIBMTP_Get_Syncpartner(self.device)).decode("utf-8")
+    #end get_sync_parner
+
+    def set_sync_partner(self, new_name) :
+        check_status(mtp.LIBMTP_Set_Syncpartner(self.device, new_name.encode("utf-8")))
+    #end set_sync_partner
+
+    def get_battery_level(self) :
+        val1 = ct.c_int(0)
+        val2 = ct.c_int(0)
+        check_status(mtp.LIBMTP_Get_Batterylevel(self.device, ct.byref(val1), ct.byref(val2)))
+        return val1.value, val2.value
+    #end get_battery_level
+
+    def get_secure_time(self) :
+        result = ct.c_char_p()
+        check_status(mtp.LIBMTP_Get_Secure_Time(self.device, ct.byref(result)))
+        return bytes(result)
+    #end get_secure_time
+
+    def get_device_certificate(self) :
+        result = ct.c_char_p()
+        check_status(mtp.LIBMTP_Get_Device_Certificate(self.device, ct.byref(result)))
+        return bytes(result)
+    #end get_device_certificate
+
+    def get_supported_filetypes(self) :
+        types = ct.POINTER(ct.c_uint16)()
+        nrtypes = ct.c_uint16(0)
+        check_status(mtp.LIBMTP_Get_Supported_Filetypes(self.device, ct.byref(types), ct.byref(nrtypes)))
+        result = []
+        for i in range(0, nrtypes.value) :
+            result.append \
+              (
+                (
+                    types[i],
+                    bytes(mtp.LIBMTP_Get_Filetype_Description(filetype_t(types[i]))).decode("utf-8")
+                )
+              )
+        #end for
+        return result
+    #end get_supported_filetypes
+
+#end Device
+
+def get_connected_devices(refresh = True) :
+    global connected_devices
+    if refresh or not bool(connected_devices) :
+        if bool(connected_devices) :
+            mtp.LIBMTP_Release_Device_List(connected_devices)
+            connected_devices = ct.POINTER(mtpdevice_t)()
+        #end if
+        check_status(mtp.LIBMTP_Get_Connected_Devices(ct.byref(connected_devices)))
+    #end if
+    dev = connected_devices
+    result = []
+    while bool(dev) :
+        result.append(Device(dev))
+        dev = dev.contents.next
     #end while
-    mtp.LIBMTP_Release_Device_List(devices)
     return result
-#end Get_Connected_Devices
+#end get_connected_devices
